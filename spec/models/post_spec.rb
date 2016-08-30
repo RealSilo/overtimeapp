63119d04a6b09615bb63a96356802715f9ebb68a
RealSilo <szilard.magyar@gmail.com>
2016-08-30 19:39:09 +0200

    adding posts index

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+  describe "creation" do
+    before do
+      @post = Post.create(date: Date.today, rationale: "testing post")
+    end
+
+    it "can be created" do
+      expect(@post).to be_valid
+    end
+
+    it "can not be created without date" do
+      @post.date = nil
+      expect(@post).to_not be_valid
+    end
+
+    it "can not be creted without rationale" do
+      @post.rationale = nil
+      expect(@post).to_not be_valid
+    end
+  end
+end

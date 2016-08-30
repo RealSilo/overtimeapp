1127444ece14c8d56e9216314f9b9edc6c40c9aa
RealSilo <szilard.magyar@gmail.com>
2016-08-30 23:11:28 +0200

    posts index view

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+@user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "John", last_name: "Doe")
+
 100.times do |post|
-  Post.create(date: Faker::Date.between(2.months.ago, Date.today), rationale: Faker::Lorem.sentence)
+  Post.create(date: Faker::Date.between(2.months.ago, Date.today), rationale: Faker::Lorem.sentence, user_id: @user.id)
 end

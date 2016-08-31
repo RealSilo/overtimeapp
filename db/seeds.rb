44ef83108b4490b3277f6b53a1699f6d41016114
RealSilo <szilard.magyar@gmail.com>
2016-08-31 17:16:18 +0200

    editing post status

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 @user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "John", last_name: "Doe")
+AdminUser.create(email: "admin@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Peter", last_name: "Thief")
 
 100.times do |post|
   Post.create(date: Faker::Date.between(2.months.ago, Date.today), rationale: Faker::Lorem.sentence, user_id: @user.id)
